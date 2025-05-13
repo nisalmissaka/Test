62242e6421c0a8027ade5cec3caac21c036ba73f
nisalmissaka <wishwakanisal@gmail.com>
2025-05-13 12:26:25 +0530

    first-commit

---
+++ Employee_Service/src/main/java/edu/icet/entity/EmployeeEntity.java
@@ -0,0 +1,42 @@
+package edu.icet.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import edu.icet.util.DepartmentType;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employee")
+public class EmployeeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private DepartmentType department;
+
+    @Column(nullable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ Employee_Service/src/main/java/edu/icet/util/DepartmentType.java
@@ -0,0 +1,5 @@
+package edu.icet.util;
+
+public enum DepartmentType {
+    HR,IT,FINANCE,OPERATIONS
+}
